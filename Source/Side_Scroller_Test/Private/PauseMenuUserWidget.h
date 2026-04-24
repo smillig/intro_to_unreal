2b4b6176d872ccf36e5aad3fe8652e13224996b6
Shaun Milligan <smillig@gmail.com>
2026-04-24 21:14:12 +0200

    Prep pause widget

--- Source/Side_Scroller_Test/Private/PauseMenuUserWidget.h
+++ Source/Side_Scroller_Test/Private/PauseMenuUserWidget.h
@@ -6,12 +6,20 @@
 #include "Blueprint/UserWidget.h"
 #include "PauseMenuUserWidget.generated.h"
 
-/**
- * 
- */
+class UButton;
+
 UCLASS()
 class UPauseMenuUserWidget : public UUserWidget
 {
 	GENERATED_BODY()
 	
+public:	
+	UPROPERTY(meta = (BindWidget))
+	UButton* Button_ResumeGame;
+	
+	UPROPERTY(meta = (BindWidget))
+	UButton* Button_ToMainMenu;
+	
+	UPROPERTY(meta = (BindWidget))
+	UButton* Button_QuitGame;
 };
